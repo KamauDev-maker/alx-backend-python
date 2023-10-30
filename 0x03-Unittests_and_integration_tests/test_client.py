d274813ef8a69c2c7c4bcc689bd6118b1a29ec76
KamauDev-maker <oscarnjenga@gmail.com>
2023-10-30 18:10:54 +0000

    To test github client org

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Test client module
+"""
+import unittest
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    Test for Github org client's
+    """
+    @parameterized.expand([
+        ('google', 'google'),
+        ('abc', 'abc'),
+    ])
+    @patch('client.get_json')
+    def test_org(
+            self,
+            org_name: str,
+            expected: str,
+            mock_get_json: Mock
+    ):
+        """
+        Method that retrieves the desired results
+        """
+        client = GithubOrgClient(org_name)
+        mock_get_json.return_value = expected
+        result = client.org
+        mock_get_json.assert_called_once_with(
+                f'https://api.github.com/orgs/{org_name}'
+        )
+        self.assertEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
