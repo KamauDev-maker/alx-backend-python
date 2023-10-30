90c520c7dc95c59fbbbf3b3faa4041a7ac8afb87
KamauDev-maker <oscarnjenga@gmail.com>
2023-10-30 16:01:02 +0000

    test unit for access nested map for exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,7 +22,6 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-
     def test_access_nested_map(
             self,
             nested_map: Dict[str, Any],
@@ -39,7 +38,6 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), "Key not found: 'a' in {}"),
         ({"a": 1}, ("a", "b"), "Key not found: 'b' in {'a': 1}")
     ])
-
     def test_access_nested_map_exception(
             self,
             nested_map: Dict[str, Any],
@@ -52,7 +50,6 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), expected_exception_message)
-
 
 
 if __name__ == '__main__':
